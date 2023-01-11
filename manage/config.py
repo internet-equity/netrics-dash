49f9272f319f2d4cac97c9d262ee0bae08f8eeb2
Jesse London <jesselondon@gmail.com>
2023-01-11 16:24:12 -0600

    fix: measurementlab images on docker hub not compiled for aarch64
    updates *our* ndt-server base image and makes this the base for
    ndt-server-full to ensure proper compilation for all target platforms

--- manage/config.py
+++ manage/config.py
@@ -4,7 +4,7 @@ import pathlib
 BINFMT_TAG = 'a7996909642ee92942dcd6cff44b9b95f08dad64'
 BINFMT_TARGET = pathlib.Path('/proc/sys/fs/binfmt_misc/qemu-aarch64')
 
-NDT_SERVER_TAG = 'v0.20.6'
+NDT_SERVER_TAG = 'v0.20.17'
 NDT_SERVER_ORIGIN = 'm-lab/ndt-server'
 
 NETRICS_USER = 'ubuntu'
